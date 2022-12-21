8b0444db5af9473f7f71d7d985ba5d098fe9329f
Christoph Mellauner <c.mellauner@gmail.com>
2022-12-21 08:20:12 -0600

    add testimonial slider

--- main.js
+++ main.js
@@ -1,4 +1,33 @@
-import 'bootstrap/dist/css/bootstrap-grid.css'
-import 'bootstrap/dist/css/bootstrap-utilities.css'
-import './style.scss'
+// BOOTSTRAP START
+import "bootstrap/dist/css/bootstrap-grid.css";
+import "bootstrap/dist/css/bootstrap-utilities.css";
+import "./style.scss";
+// BOOTSTRAP END
 
+// AOS (animate on scroll) START
+import Aos from "aos";
+Aos.init();
+// AOS (animate on scroll) END
+
+// SWIPER START
+
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from "swiper";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// init Swiper:
+const testimonialsSwiper = new Swiper(".testimonial-swiper", {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+    spaceBetween: 30,
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+// SWIPER END
