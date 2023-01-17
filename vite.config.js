f0bdd4b9ac5a33ed91555aae099b71762a8c4172
Christoph Mellauner <c.mellauner@gmail.com>
2023-01-16 20:59:59 -0600

    add new sites and selfhost fonts

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,16 @@
-// vite.config.js
-export default {
-    // config options
-  }
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                "about-us": resolve(__dirname, "about-us/index.html"),
+                impressum: resolve(__dirname, "impressum/index.html"),
+                "ueber-uns": resolve(__dirname, "ueber-uns/index.html"),
+                datenschutz: resolve(__dirname, "datenschutz/index.html"),
+            },
+        },
+    },
+});
