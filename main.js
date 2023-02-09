5c2fefb2c4dd87a9634cec69b12a1df6371a2f71
Christoph Mellauner <c.mellauner@gmail.com>
2023-02-09 07:25:23 -0500

    add new webp images and removed swiper js for now

--- main.js
+++ main.js
@@ -17,11 +17,11 @@ Aos.init();
 // SWIPER START
 
 // core version + navigation, pagination modules:
-import Swiper, { Navigation, Pagination } from "swiper";
-// import Swiper and modules styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+// import Swiper, { Navigation, Pagination } from "swiper";
+// // import Swiper and modules styles
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
 
 document.addEventListener("DOMContentLoaded", function () {
     // init Swiper:
