1529f444bd2fcb0c354b8bf23c692c5a8800061b
christophmellauner <c.mellauner@gmail.com>
2023-12-22 09:30:26 +0100

    add sitemap manually

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import injectHTML from "vite-plugin-html-inject";
-import Sitemap from 'vite-plugin-sitemap'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 
 export default defineConfig({
     plugins: [
         ViteImageOptimizer({}),
-        Sitemap({
-            hostname: "https://www.calendall.net"
-        }),
         injectHTML({
             debug: {
                 logPath: true,
@@ -21,18 +17,18 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                impressum: resolve(__dirname, "impressum/index.html"),
-                "ueber-uns": resolve(__dirname, "ueber-uns/index.html"),
-                datenschutz: resolve(__dirname, "datenschutz/index.html"),
-                kontakt: resolve(__dirname, "kontakt/index.html"),
-                agb: resolve(__dirname, "agb/index.html"),
-                friseursalon: resolve(__dirname, "friseursalon/index.html"),
-                kosmetikstudio: resolve(__dirname, "kosmetikstudio/index.html"),
-                barbershop: resolve(__dirname, "barbershop/index.html"),
-                nagelstudio: resolve(__dirname, "nagelstudio/index.html"),
-                massagestudio: resolve(__dirname, "massagestudio/index.html"),
-                fusspflege: resolve(__dirname, "fusspflege/index.html"),
-                preise: resolve(__dirname, "preise/index.html"),
+                "ueber-uns/": resolve(__dirname, "ueber-uns/index.html"),
+                "impressum/": resolve(__dirname, "impressum/index.html"),
+                "datenschutz/": resolve(__dirname, "datenschutz/index.html"),
+                "kontakt/": resolve(__dirname, "kontakt/index.html"),
+                "agb/": resolve(__dirname, "agb/index.html"),
+                "friseursalon/": resolve(__dirname, "friseursalon/index.html"),
+                "kosmetikstudio/": resolve(__dirname, "kosmetikstudio/index.html"),
+                "barbershop/": resolve(__dirname, "barbershop/index.html"),
+                "nagelstudio/": resolve(__dirname, "nagelstudio/index.html"),
+                "massagestudio/": resolve(__dirname, "massagestudio/index.html"),
+                "fusspflege/": resolve(__dirname, "fusspflege/index.html"),
+                "preise/": resolve(__dirname, "preise/index.html"),
             },
         },
     },
